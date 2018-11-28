9c3f7353ed0074625edc3db21c3344bd013db195
Mamadou Diallo <mamadou.diallo@wework.com>
2018-11-28 04:16:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  let str = ""
+  for(var i = 0;i<dwarves.length;i++){
+  var name = dwarves[i]
+  var number = i+1
+  str = str +(number)+ ". "+(name)+" "
+   
+  }
+  return str
+  
+}
+ 
+function summonCaptainPlanet(planeteerCalls){
+  let container = []
+  for(var i = 0; i<planeteerCalls.length; i++){
+    var planeteerCall = planeteerCalls[i]
+    var message = planeteerCall.toUpperCase() +"!"
+    
+    container.push(message)
+    //container.push(planeteerCalls[i].toUpperCase() + "!")
+
+    
+    
+  }
+  return container
 }
 
-function summonCaptainPlanet(planeteerCalls){
-}
 
 function longPlaneteerCalls(words) {
+  let str = []
+  for(var i = 0;i<words.length;i++){
+    if(words.length<4)return false
+  }
+  return true
 }
 
 function findTheCheese (foods) {
+  var str = []
+  for(var i =0; i<foods.length;i++){
+    var item = foods[i]
+    
+    if(item ==="cheddar") return "cheddar"
+  }
+  return  "no cheese!"
 }
